b73d3adec92f229cb7dd96e19761a17bb19a15c8
Jay Versluis <versluis2000@yahoo.com>
2019-04-14 11:20:34 -0400

    passed user specified class to jQuery

--- vanisher.js
+++ vanisher.js
@@ -4,8 +4,14 @@
 // print jQuery version
 // alert ("We're rocking jQuery Version " + jQuery.fn.jquery);
 
+// print a variable from PHP
+// alert ('Random Value from PHP is ' + elv_data.random_value);
+
+// print a concatenated string
+$elv_vanish_class = elv_data.elv_class + ' a';
+
 // vanish empty links
-jQuery('.one a').each(function() {
+jQuery($elv_vanish_class).each(function() {
     if(!jQuery(this).attr('href')) {
         jQuery(this).parent().hide();      
 	} 
